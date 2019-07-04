c4f8ed40e0097cb06f5060462823a0792db1c6fe
Teresa Kamau <51070829+tessa5@users.noreply.github.com>
2019-07-04 19:05:39 +0300

    updated profile
    did an update on my portfolio and added some few files, made some changes but i have some errors in .js

---
+++ js/index.js
@@ -0,0 +1,134 @@
+ $(function(){
+ var navPos = $('nav').position().top;
+  var lastPos = 0;
+  var lockTimer
+
+  $(window).on('scroll', function () {
+
+    var pos = $(window).scrollTop();
+    var pos2 = pos + 50;
+    var scrollBottom = pos + $(window).height();
+
+    if (!isMobile) {
+      if (pos >= navPos + $('nav').height() && lastPos < pos) {
+        $('nav').addClass('fixed');
+      }
+      if (pos < navPos && lastPos > pos) {
+        $('nav').removeClass('fixed');
+      }
+      lastPos = pos;
+    }
+
+    // Link Highlighting
+    if (pos2 > $('#home').offset().top)       { highlightLink('home'); }
+    if (pos2 > $('#about').offset().top)      { highlightLink('about'); }
+    if (pos2 > $('#profile').offset().top)  { highlightLink('profile'); }
+    if (pos2 > $('#contact').offset().top ||
+        pos + $(window).height() === $(document).height()) {
+          highlightLink('contact');
+    }
+  });
+
+  // Prevent Hover on Scroll
+    clearTimeout(lockTimer);
+    if(!$('body').hasClass('disable-hover')) {
+      $('body').addClass('disable-hover')
+    }
+
+    lockTimer = setTimeout(function(){
+      $('body').removeClass('disable-hover')
+    }, 500);
+  });
+
+  function highlightLink(anchor) {
+    $('nav .active').removeClass('active');
+    $("nav").find('[dest="' + anchor + '"]').addClass('active');
+  }
+
+
+  // EVENT HANDLERS
+  $('.page-link').click(function() {
+    var anchor = $(this).attr("dest");
+    $('.link-wrap').removeClass('visible');
+
+    $('nav span').removeClass('active');
+    $("nav").find('[dest="'+ anchor +'"]').addClass('active');
+
+    $('html, body').animate({
+      scrollTop: $('#' + anchor).offset().top
+    }, 400);
+  });
+  function onScrollInit( items, elemTrigger ) {
+    var offset = $(window).height() / 1.6
+    items.each( function() {
+      var elem = $(this),
+          animationClass = elem.attr('data-animation'),
+          animationDelay = elem.attr('data-delay');
+
+          elem.css({
+            '-webkit-animation-delay':  animationDelay,
+            '-moz-animation-delay':     animationDelay,
+            'animation-delay':          animationDelay
+          });
+         
+          var trigger = (elemTrigger) ? trigger : elem;
+
+           trigger.waypoint(function() {
+            elem.addClass('animated').addClass(animationClass);
+            },{
+                triggerOnce: true,
+                offset: offset
+          });
+    });
+  }
+
+  setTimeout(function() { onScrollInit($('.waypoint')) }, 10);
+
+
+//  highchart
+Highcharts.chart('skills', {
+  chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+  },
+  title: {
+      text: 'skills breakdown'
+  },
+  tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  plotOptions: {
+      pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+              enabled: true,
+              format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+              style: {
+                  color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+              }
+          }
+      }
+  },
+  series: [{
+      name: 'User Experience',
+      colorByPoint: true,
+      data: [{
+          name: 'graphic design',
+          categories: ['CSS3', 'HTML5', 'jQuery', 'PHP', 'SQL'],
+           data: [8, 4, 3, 3, 1],
+          y: 48,
+          sliced: true,
+          selected: true
+      }, {
+          name: 'web programming',
+          y: 40
+      }, {
+          name: 'JS',
+          y: 35 
+      }]
+  }]
+});
+  
